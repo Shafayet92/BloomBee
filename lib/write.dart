a4edd7d9859ce2887045c82514689b3479f9f995
Shafayet92 <mdshafayethossain92@gmail.com>
2022-05-26 04:00:45 +0800

    666

--- lib/write.dart
+++ lib/write.dart
@@ -1,8 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'login.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(Write());
+}
 
 class Write extends StatefulWidget {
   @override
@@ -14,29 +21,57 @@ class _WriteState extends State<Write> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        backgroundColor: Colors.amber,
+        backgroundColor: Color.fromARGB(255, 201, 130, 7),
         appBar: AppBar(
           title: TextField(controller: controller),
           actions: [
             IconButton(
               icon: const Icon(Icons.add),
-              //  color: selection =Colors.green[400]!,
               onPressed: () {
-                // final name = controller.text;
-                // createUser(name: name);
+                final name = controller.text;
+                createUser(name: name);
               },
             )
           ],
         ),
       );
-  // Future createUser({required String name}) async {
-  //   final docUser = FirebaseFirestore.instance.collection('users').doc('my-id');
+  Future createUser({required String name}) async {
+    final docUser = FirebaseFirestore.instance.collection('users').doc('my-id');
 
-  //   final json = {
-  //     'name': name,
-  //     'age': 21,
-  //     'birthday': DateTime(2001, 7, 28),
-  //   };
-  //   await docUser.set(json);
-  // }
+    final user = User(
+      id: docUser.id,
+      name: name,
+      age: 21,
+      birthday: DateTime(2001, 7, 28),
+    );
+
+    final json = user.toJson();
+
+    // final json = {
+    //   'name': name,
+    //   'age': 21,
+    //   'birthday': DateTime(2001, 7, 28),
+    // };
+    await docUser.set(json);
+  }
+}
+
+class User {
+  String id;
+  final String name;
+  final int age;
+  final DateTime birthday;
+
+  User({
+    this.id = '',
+    required this.name,
+    required this.age,
+    required this.birthday,
+  });
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'age': age,
+        'birthday': birthday,
+      };
 }
