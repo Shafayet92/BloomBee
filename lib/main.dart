a40c2146964cd0dd7cc437523431ad51b32b32c0
Shafayet92 <mdshafayethossain92@gmail.com>
2022-05-26 03:34:12 +0800

    up2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,58 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+
+// import 'register.dart';
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   final controller = TextEditingController();
+
+//   @override
+//   Widget build(BuildContext context) => Scaffold(
+//         backgroundColor: Colors.amber,
+//         appBar: AppBar(
+//           title: TextField(controller: controller),
+//           actions: [
+//             IconButton(
+//               icon: const Icon(Icons.add),
+//               //  color: selection =Colors.green[400]!,
+//               onPressed: () {
+//                 // final name = controller.text;
+//                 // createUser(name: name);
+//               },
+//             )
+//           ],
+//         ),
+//       );
+//   // Future createUser({required String name}) async {
+//   //   final docUser = FirebaseFirestore.instance.collection('users').doc('my-id');
+
+//   //   final json = {
+//   //     'name': name,
+//   //     'age': 21,
+//   //     'birthday': DateTime(2001, 7, 28),
+//   //   };
+//   //   await docUser.set(json);
+//   // }
+// }
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'home.dart';
 import 'register.dart';
+import 'write.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +73,44 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primaryColor: Colors.blue[900],
       ),
-      home: Register(),
+      home: Write(),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+
+// import 'register.dart';
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primaryColor: Colors.blue[900],
+//       ),
+//       home: Register(),
+//     );
+//   }
+// }
--- lib/write.dart
+++ lib/write.dart
@@ -1,44 +1,42 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+import 'login.dart';
+
+class Write extends StatefulWidget {
+  @override
+  _WriteState createState() => _WriteState();
 }
 
-class MyApp extends StatelessWidget {
-// This widget is the root
-// of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Firebase',
-      home: AddData(),
-    );
-  }
-}
+class _WriteState extends State<Write> {
+  final controller = TextEditingController();
 
-class AddData extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: Text("geeksforgeeks"),
-      ),
-      body: Center(
-        child: FloatingActionButton(
-          backgroundColor: Colors.green,
-          child: Icon(Icons.add),
-          onPressed: () {
-            FirebaseFirestore.instance
-                .collection('data')
-                .add({'text': 'data added through app'});
-          },
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Colors.amber,
+        appBar: AppBar(
+          title: TextField(controller: controller),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.add),
+              //  color: selection =Colors.green[400]!,
+              onPressed: () {
+                // final name = controller.text;
+                // createUser(name: name);
+              },
+            )
+          ],
         ),
-      ),
-    );
-  }
+      );
+  // Future createUser({required String name}) async {
+  //   final docUser = FirebaseFirestore.instance.collection('users').doc('my-id');
+
+  //   final json = {
+  //     'name': name,
+  //     'age': 21,
+  //     'birthday': DateTime(2001, 7, 28),
+  //   };
+  //   await docUser.set(json);
+  // }
 }
