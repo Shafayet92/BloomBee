aa79063e4142f74061002a142410f8dbafd3f05f
Shafayet92 <mdshafayethossain92@gmail.com>
2022-07-03 08:21:19 +0800

    up2

--- lib/Home_Page/screens/DarkMood/settings.dart
+++ lib/Home_Page/screens/DarkMood/settings.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:register/Home_Page/screens/DarkMood/settings_theme.dart';
 
 final darkNotifier = ValueNotifier<bool>(false);
 
@@ -32,5 +31,47 @@ class _SettingsPageState extends State<SettingsPage> {
     //     body: new Center(
     //       child: new Text("This is Settings Add night mode here"),
     //     ));
+  }
+}
+
+class SettingsPages extends StatefulWidget {
+  SettingsPages({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _SettingsPagesState createState() => _SettingsPagesState();
+}
+
+class _SettingsPagesState extends State<SettingsPages> {
+  // @override
+  // void dispose() {
+  //   // TODO: implement dispose
+  //   darkNotifier.dispose();
+  //   super.dispose();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    bool isDark = darkNotifier.value;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          isDark = !isDark;
+          darkNotifier.value = isDark;
+        },
+        tooltip: 'Increment',
+        child: Icon(isDark ? Icons.wb_sunny_outlined : Icons.bubble_chart),
+      ),
+    );
   }
 }
